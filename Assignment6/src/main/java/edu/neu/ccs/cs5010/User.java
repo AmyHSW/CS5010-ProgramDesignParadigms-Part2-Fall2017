7269a2eb77718c3abe6271128fe27337d8d3deb0
jingyus <jingyus@uw.edu>
2017-11-06 18:01:34 -0800

    user

--- Assignment6/src/main/java/edu/neu/ccs/cs5010/User.java
+++ Assignment6/src/main/java/edu/neu/ccs/cs5010/User.java
@@ -3,41 +3,80 @@ package edu.neu.ccs.cs5010;
 import java.time.LocalDate;
 
 public class User implements IUser {
+  private int userId;
+  private LocalDate createdDate;
+  private char gender;
+  private int age;
+  private String city;
+  private int numFollowers;
+  private int numFollowees;
 
-  public User(int userId, LocalDate date, char gender, int age, String city) {}
+  public User(int userId, LocalDate createdDate, char gender, int age, String city) {
+    this.userId = userId;
+    this.createdDate = createdDate;
+    this.gender = gender;
+    this.age = age;
+    this.city = city;
+  }
 
   @Override
   public int getUserId() {
-    return 0;
+    return userId;
   }
 
   @Override
   public void addOneFollower() {
-
+    numFollowers++;
   }
 
   @Override
   public void addOneFollowee() {
-
+    numFollowees++;
   }
 
   @Override
   public int getNumFollowees() {
-    return 0;
+    return numFollowees;
   }
 
   @Override
   public int getNumFollowers() {
-    return 0;
+    return numFollowers;
   }
 
   @Override
   public LocalDate getCreatedDate() {
-    return null;
+    return createdDate;
   }
 
   @Override
   public int compareTo(IUser that) {
     return this.getUserId() - that.getUserId();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+
+    User user = (User) other;
+
+    return userId == user.userId;
+  }
+
+  @Override
+  public int hashCode() {
+    return userId;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" + "userId=" + userId + ", createdDate=" + createdDate + ", gender=" + gender
+            + ", age=" + age + ", city='" + city + ", numFollowers=" + numFollowers +
+            ", numFollowees=" + numFollowees + '}';
   }
 }
